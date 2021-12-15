0abb90f810477bdeb1dabc8b412fbf988af6b035
Seungjin Kim <skim@proove.com>
2021-12-15 08:46:14 -0800

    initial commit

---
+++ lib/probabilistic_book_review.ex
@@ -0,0 +1,18 @@
+defmodule ProbabilisticBookReview do
+  @moduledoc """
+  Documentation for `ProbabilisticBookReview`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ProbabilisticBookReview.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/probabilistic_book_review_test.exs
@@ -0,0 +1,8 @@
+defmodule ProbabilisticBookReviewTest do
+  use ExUnit.Case
+  doctest ProbabilisticBookReview
+
+  test "greets the world" do
+    assert ProbabilisticBookReview.hello() == :world
+  end
+end
